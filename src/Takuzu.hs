fb399e3fe9676a814f1f4ce655004657e4344558
atrolla <yvan.vu@arolla.fr>
2016-08-19 10:04:13 +0200

    personal work with some optim attempts

--- src/Takuzu.hs
+++ src/Takuzu.hs
@@ -17,24 +17,77 @@ isBlank = (==blank)
 symbols :: [Digit]
 symbols = ['0','1']
 
--- solve
+--
 
 solve :: Grid -> Grid
 solve = head . filter valid . combine . choices
+
+solve2 :: Grid -> Grid
+solve2 = head . checkvalid . combine2 . choices
+
+-- deduce
+
+deduce :: Matrix Choices -> Matrix Choices
+deduce = map fill
+
+fill :: [Choices] -> [Choices]
+fill (x:y:z:xs)
+  | x == y && (not (isSingleton z)) && isSingleton x = fill (x:x:(inv x):xs)
+  | x == z && (not (isSingleton y)) && isSingleton x = fill (x:(inv x):x:xs)
+  | y == z && (not (isSingleton x)) && isSingleton y = fill ((inv z):z:z:xs)
+  | otherwise = x:(fill (y:z:xs))
+fill l = l
+
+isSingleton :: Choices -> Bool
+isSingleton [_] = True
+isSingleton _ = False
+
+inv :: Choices -> Choices
+inv ['0'] = ['1']
+inv _ = ['0']
 
 -- choices
 
 type Choices = [Digit]
 
 choices :: Grid -> Matrix Choices
-choices = undefined
+choices = map (map possibilities)
+  where possibilities x | isBlank x = symbols
+                        | otherwise = [x]
+
 
 -- combine
 
 combine :: Matrix Choices -> [Grid]
-combine = undefined
+combine = sequence . map sequence
 
+combine2 :: Matrix Choices -> [Grid]
+combine2 = sequence . map (optim . sequence)
+  where optim = filter isBalanced . filter noTriplicate
+  
 -- valid
+isFilled :: [Digit] -> Bool
+isFilled [] = False
+isFilled l = blank `notElem` l
+
+isBalanced :: [Digit] -> Bool
+isBalanced []  = False
+isBalanced l  = numberOf '1' l == numberOf '0' l
+  where numberOf c xs = length $ filter (==c) xs
+
+noTriplicate :: [Digit] -> Bool
+noTriplicate (x:y:z:xs) = not triplicate && noTriplicate l
+  where l = y:z:xs
+        triplicate = (x == y && y == z)
+noTriplicate _ = True
+
+isUnique :: Grid -> Bool
+isUnique [] = True
+isUnique ( x : xs ) = all ( /= x ) xs && isUnique xs
 
 valid :: Grid -> Bool
-valid g = undefined
+valid g = all (isFilled) g && check g && check (transpose g)
+
+check x = isUnique x && all (isBalanced) x && all (noTriplicate) x
+
+checkvalid = filter (check . transpose) . filter check . filter (all isFilled)
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -9,4 +9,65 @@ main = hspec $ do
   describe "choices does" $ do
     it "try all combination of possible symbol for blank entries" $ do
       choices [[]] `shouldBe` [[]]
+      choices [['1']] `shouldBe` [[['1']]]
+      choices [['.']] `shouldBe` [[['0','1']]]
+      choices [['.','1','1','0']] `shouldBe` [[['0','1'],['1'],['1'],['0']]]
       
+  --describe "combine does"
+    --it "combine choices to grids"
+      
+      
+  describe "valid does" $ do
+    describe "check that a Takuzu" $ do
+      it "is filled" $ do
+        isFilled [] `shouldBe` False
+        isFilled ['1'] `shouldBe` True
+        isFilled ['.'] `shouldBe` False
+        isFilled ['.','1'] `shouldBe` False
+        isFilled ['.','0'] `shouldBe` False
+        
+      it "contains an equal number of '1' and '0'" $ do
+        isBalanced [] `shouldBe` False
+        isBalanced ['0','1'] `shouldBe` True
+        isBalanced ['0','1','0','1'] `shouldBe` True
+        isBalanced ['0','1','1'] `shouldBe` False
+        
+      it "has not 3 same and consecutives number" $ do
+        noTriplicate [] `shouldBe` True
+        noTriplicate ['1','1','1'] `shouldBe` False
+        noTriplicate ['1','1','1','0'] `shouldBe` False
+        noTriplicate ['0','1','1','1','0'] `shouldBe` False
+        noTriplicate ['0','1','1','0','0'] `shouldBe` True
+  
+      it "has each row unique" $ do
+        isUnique [[]] `shouldBe` True
+        isUnique [['0'],['1']] `shouldBe` True
+    
+    it "OK" $ do      
+      valid [[]]  `shouldBe` False
+      valid [['0','1','1','0'],['1','0','0','1'],['0','0','1','1'],['1','1','0','0']]  `shouldBe` True
+      valid [['0','0','1','1'],['1','1','0','0'],['0','0','1','1'],['1','1','0','0']]  `shouldBe` False
+      valid [['.','1','1','0'],['1','0','0','1'],['0','0','1','1'],['1','1','0','0']]  `shouldBe` False
+      
+  describe "solve does" $ do
+    it "use all previous functions to find a solution for a Takuzu" $ do
+      solve [['.','1','1','0'],['1','0','0','1'],['0','0','1','1'],['1','1','0','0']]  `shouldBe` [['0','1','1','0'],['1','0','0','1'],['0','0','1','1'],['1','1','0','0']]
+      
+  describe "deduce does" $ do
+    describe "fill does" $ do
+      it "replace choices with the only valid possible symbol" $ do
+        fill [['0','1'],['0'],['0']] `shouldBe` [['1'],['0'],['0']]
+        fill [['0'],['0','1'],['0']] `shouldBe` [['0'],['1'],['0']]
+        fill [['0'],['0','1'],['0'],['0']] `shouldBe` [['0'],['1'],['0'],['0']]
+        fill [['0'],['0'],['0','1'],['0'],['0']] `shouldBe` [['0'],['0'],['1'],['0'],['0']]
+  
+    it "OK" $ do
+      deduce [[['0','1'],['0'],['0']]] `shouldBe` [[['1'],['0'],['0']]]
+      
+  describe "solve2 does" $ do
+    it "use all previous functions to find a solution for a Takuzu" $ do
+      solve2 [['.','1','1','0'],['1','0','0','1'],['0','0','1','1'],['1','1','0','0']]  `shouldBe` [['0','1','1','0'],['1','0','0','1'],['0','0','1','1'],['1','1','0','0']]
+      
+  describe "checkvalid" $ do
+    it "is like filter valid" $ property $
+      \g -> checkvalid g == filter valid g
