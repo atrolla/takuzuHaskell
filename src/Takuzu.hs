08d81e72a89c9df89394092e8373cadf01d2facb
atrolla <yvan.vu@arolla.fr>
2016-08-22 07:35:35 +0200

    optim attempt

--- src/Takuzu.hs
+++ src/Takuzu.hs
@@ -3,7 +3,8 @@ module Takuzu where
 import Data.List
 -- types
 
-type Matrix a = [[a]]
+type Matrix a = [Row a]
+type Row a = [a]
 type Digit = Char
 
 type Grid = Matrix Digit
@@ -22,19 +23,65 @@ symbols = ['0','1']
 solve :: Grid -> Grid
 solve = head . filter valid . combine . choices
 
+solve2 :: Grid -> Grid
+solve2 g = head $ intersect solveByRow solveByCol
+    where solveByRow = (combine . choices) g
+          solveByCol = (map transpose . combine . choices . transpose) g
+
 -- choices
 
-type Choices = [Digit]
+type Choices = Row Digit
 
 choices :: Grid -> Matrix Choices
-choices = undefined
+choices = map (rowChoices)
+
+rowChoices :: Choices -> Row Choices
+rowChoices r = nub $ map (convert r) allPossibilities
+    where allPossibilities = permutations $ possibilities r 
+
+convert :: Choices -> Choices -> Choices
+convert r p 
+    | r == [] = []
+    | rhIsBlank = ph : convert rt pt
+    | not rhIsBlank = rh : convert rt p
+    | otherwise = [] -- not necessary
+        where rhIsBlank = isBlank rh
+              (rh:rt) = r
+              (ph:pt) = p
+
+possibilities :: Choices -> Choices
+possibilities r = concatMap (\s -> replicate (remaining s) s) symbols
+    where   remaining x = halfLength - occurences x
+            occurences x = length $ filter (==x) r
+            halfLength = (length r) `div` 2
 
 -- combine
 
 combine :: Matrix Choices -> [Grid]
-combine = undefined
+combine = sequence
 
 -- valid
+isFilled :: [Digit] -> Bool
+isFilled [] = False
+isFilled l = blank `notElem` l
+
+isBalanced :: [Digit] -> Bool
+isBalanced []  = False
+isBalanced l  = numberOf '1' l == numberOf '0' l
+  where numberOf c xs = length $ filter (==c) xs
+
+noTriplicate :: [Digit] -> Bool
+noTriplicate (x:y:z:xs) = not triplicate && noTriplicate l
+  where l = y:z:xs
+        triplicate = (x == y && y == z)
+noTriplicate _ = True
+
+isUnique :: Grid -> Bool
+isUnique [] = True
+isUnique ( x : xs ) = all ( /= x ) xs && isUnique xs
 
 valid :: Grid -> Bool
-valid g = undefined
+valid = check [all (isFilled), row, row . transpose]
+    where row = check [isUnique, all (isBalanced), all (noTriplicate)]
+          check l x = foldl (&&) True $ map (\f -> f x) l
+          
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -3,10 +3,61 @@ module Main where
 import Test.Hspec
 import Test.QuickCheck
 import Takuzu
+import Data.List
 
 main :: IO()
 main = hspec $ do
   describe "choices does" $ do
-    it "try all combination of possible symbol for blank entries" $ do
-      choices [[]] `shouldBe` [[]]
-      
+    describe "possibilities" $ do
+        it "returns all remaining symbols for blank entries of a row" $ do
+            possibilities ['.','.'] `shouldBe` "01"
+            possibilities ['0','.'] `shouldBe` "1"
+            possibilities ['0','.','0','.','.','.'] `shouldBe` "0111"
+    describe "convert" $ do
+        it "replace blank entries with a possibility" $ do
+            convert ['0'] [] `shouldBe` ['0']
+            convert ['.'] ['1'] `shouldBe` ['1']
+            convert ['0','.'] ['1'] `shouldBe` ['0','1']
+            convert ['.','0'] ['1'] `shouldBe` ['1','0']
+    describe "rowChoices" $ do
+        it "try all combination of possibilities for a row with blank entries" $ do
+            rowChoices ['0'] `shouldBe` [['0']]
+            rowChoices ['0','.'] `shouldBe` [['0','1']]
+            rowChoices ['1','.'] `shouldBe` [['1','0']]
+            sort (rowChoices ['1','.','0','.']) `shouldBe` sort ([['1','1','0','0'],['1','0','0','1']])
+            sort (rowChoices ['.','.','0','.']) `shouldBe` sort (["0101","1001","1100"])
+    it "try all combination of possibilities on a grid" $ do
+        choices [['0']] `shouldBe` [[['0']]]
+        choices [['1','.','0','.']] `shouldBe` [sort ([['1','1','0','0'],['1','0','0','1']])]
+        
+  describe "valid does" $ do
+    describe "check that a Takuzu" $ do
+      it "is filled" $ do
+        isFilled [] `shouldBe` False
+        isFilled ['1'] `shouldBe` True
+        isFilled ['.'] `shouldBe` False
+        isFilled ['.','1'] `shouldBe` False
+        isFilled ['.','0'] `shouldBe` False
+        
+      it "contains an equal number of '1' and '0'" $ do
+        isBalanced [] `shouldBe` False
+        isBalanced ['0','1'] `shouldBe` True
+        isBalanced ['0','1','0','1'] `shouldBe` True
+        isBalanced ['0','1','1'] `shouldBe` False
+        
+      it "has not 3 same and consecutives number" $ do
+        noTriplicate [] `shouldBe` True
+        noTriplicate ['1','1','1'] `shouldBe` False
+        noTriplicate ['1','1','1','0'] `shouldBe` False
+        noTriplicate ['0','1','1','1','0'] `shouldBe` False
+        noTriplicate ['0','1','1','0','0'] `shouldBe` True
+
+      it "has each row unique" $ do
+        isUnique [[]] `shouldBe` True
+        isUnique [['0'],['1']] `shouldBe` True
+
+    it "OK" $ do      
+      valid [[]]  `shouldBe` False
+      valid [['0','1','1','0'],['1','0','0','1'],['0','0','1','1'],['1','1','0','0']]  `shouldBe` True
+      valid [['0','0','1','1'],['1','1','0','0'],['0','0','1','1'],['1','1','0','0']]  `shouldBe` False
+      valid [['.','1','1','0'],['1','0','0','1'],['0','0','1','1'],['1','1','0','0']]  `shouldBe` False 
