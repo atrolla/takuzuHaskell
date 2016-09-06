c9ace67094894a5d5406c59070a002ecb6cb9b64
atrolla <yvan.vu@arolla.fr>
2016-09-06 14:11:20 +0200

    tloop

--- src/Takuzu.hs
+++ src/Takuzu.hs
@@ -3,8 +3,9 @@ module Takuzu where
 import Data.List
 -- types
 
-type Matrix a = [[a]]
+type Matrix a = [Row a]
 type Digit = Char
+type Row a = [a]
 
 type Grid = Matrix Digit
 
@@ -20,21 +21,43 @@ symbols = ['0','1']
 -- solve
 
 solve :: Grid -> Grid
-solve = head . filter valid . combine . choices
+solve = loop (map rowRules)
+  where rowRules = deduce . fill
 
--- choices
+--
 
-type Choices = [Digit]
+deduce :: Row Digit -> Row Digit
+deduce (x:y:z:zs)
+  | y == z && not (isBlank y) && isBlank x = inv y : deduce (y:z:zs)
+  | x == z && not (isBlank x) && isBlank y = x : deduce ((inv x):z:zs)
+  | x == y && not (isBlank x) && isBlank z = x : deduce (y:(inv x):zs)
+  | otherwise                              = x : deduce (y:z:zs)
+    where inv '1' = '0'
+          inv _   = '1'
+deduce l = l
 
-choices :: Grid -> Matrix Choices
-choices = undefined
+-- fill
 
--- combine
+fill :: Row Digit -> Row Digit
+fill ls
+  | full '1'  = map (replace '0') ls
+  | full '0'  = map (replace '1') ls
+  | otherwise = ls
+  where full x = halfLength == occurence x
+        occurence x = (length $ filter (==x) ls)
+        halfLength = (length ls) `div` 2
+        replace x y | y == '.'  = x
+                    | otherwise = y
 
-combine :: Matrix Choices -> [Grid]
-combine = undefined
+-- loop
+
+loop :: (Eq a) => (a -> a) -> a -> a
+loop f x
+  | y == x = x
+  | otherwise = loop f y
+  where y = f x
 
 -- valid
 
 valid :: Grid -> Bool
-valid g = undefined
+valid _ = undefined
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -6,7 +6,27 @@ import Takuzu
 
 main :: IO()
 main = hspec $ do
-  describe "choices does" $ do
-    it "try all combination of possible symbol for blank entries" $ do
-      choices [[]] `shouldBe` [[]]
-      
+  describe "deduce does" $ do
+    it "replace a blank with the only possible symbol using triplicate rule" $ do
+      deduce "00." `shouldBe` "001"
+      deduce "0.0" `shouldBe` "010"
+      deduce ".00" `shouldBe` "100"
+      deduce ".000" `shouldBe` "1000"
+      deduce "0.00" `shouldBe` "0100"
+      deduce "00.00" `shouldBe` "00100"
+      deduce "01100" `shouldBe` "01100"
+      deduce "011.0" `shouldBe` "01100"
+  describe "fill does" $ do
+    it "replaces remaining blanks using balance rule" $ do
+      fill "0." `shouldBe` "01"
+      fill "1." `shouldBe` "10"
+      fill "1.1." `shouldBe` "1010"
+      fill "1.1..." `shouldBe` "1.1..."
+
+  describe "loop does" $ do
+    it "apply a function till the result is the same as input" $ do
+      let function x | x == 3 = x
+                     | otherwise = x + 1
+          func2 x | head x == 'b' = x
+                  | otherwise = tail x
+      loop func2 "qsmldkfjbqsdf" `shouldBe` "bqsdf"
